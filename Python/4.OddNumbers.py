1ae9245fe590903e5d4187e8e05d0faa7b06aedb
carlesgl81 <36437707+carlesgl81@users.noreply.github.com>
2019-10-12 18:58:02 -0400

    Add files via upload

---
+++ Python/4.OddNumbers.py
@@ -0,0 +1,26 @@
+# Print odd numbers between 1 and 100	Between two given numbers
+print("Add 2 numbers between 1 and 100: ")
+num1 = int(input("Add the first number: "))
+num2 = int(input("Add the second number: "))
+
+
+#This function checks if number 1 is smaller than number 2.
+def lessthannum2():
+    operation = num
+    while (operation < num2):
+        print (operation)
+        operation = operation + 2
+    else:
+        print("There are no more odd numbers between ",num1," and ",num2)
+        
+#This statement checks if number 1 is odd. If not it adds 1.
+if num1 % 2 == 0: 
+    num = (num1 + 1)
+else:
+    num = num1
+
+lessthannum2()
+
+
+
+
